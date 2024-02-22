caf125297b71c08a792b440d99a97c7d6fe711da
Jack <Paulsjac000@gmail.com>
2024-02-21 23:12:31 -0500

    pushing styling for about me page and adding styles

---
+++ aboutMePage/aboutMe.js
@@ -0,0 +1,55 @@
+
+// Attach the function to the onload event
+window.addEventListener('load', getime);
+
+function getime()
+{
+    // Create a new Date object
+    var currentDate = new Date();
+
+    // Get the current date
+    var day = currentDate.getDate();
+    var month = currentDate.getMonth() + 1; // Months are zero-indexed, so we add 1
+    var year = currentDate.getFullYear();
+
+    // Get the current time
+    var hours = currentDate.getHours();
+    var minutes = currentDate.getMinutes();
+    var seconds = currentDate.getSeconds();
+
+
+    console.log(hours);
+    // Format the date and time
+    var formattedDate = `${year}-${month}-${day}`;
+    var formattedTime = `${hours}:${minutes}:${seconds}`;
+
+    // Output the current date and time
+    // console.log("Current Date:", formattedDate);
+    // console.log("Current Time:", formattedTime);
+
+    var timeMessage;
+
+    if (hours >= 0 && hours <= 4)
+    {
+        timeMessage = "good night";
+    }
+    else if (hours >= 4 && hours < 12)
+    {
+        timeMessage = "good morning";
+    }
+    else if (hours >= 12 && hours < 17)
+    {
+        timeMessage = "good afternoon"; 
+    }
+    else if (hours >= 17 && hours < 21)
+    {
+        timeMessage = "good evening";
+    }
+    else if (hours >=21)
+    {
+        timeMessage = "good night";
+    }
+
+    var welcomeMessage= document.getElementById("welcomeMessageID");
+    welcomeMessage.innerHTML = "Welcome to my about me page! <br>I hope you are having a " + timeMessage + "!";
+}
