9782ceb2652ad0ede6a18934c352c98587911bbe
Jack <Paulsjac000@gmail.com>
2024-03-04 00:17:41 -0500

    adding stockpage.js. Text now updates based on the year. other small cosmetic changes

--- index.js
+++ index.js
@@ -12,30 +12,3 @@ function openLinkInNewTab(url) {
     
     a.dispatchEvent(evt);
 }
-
-// Attach the function to the onload event
-// window.addEventListener('load', getime);
-
-// function getime()
-// {
-//     // Create a new Date object
-//     var currentDate = new Date();
-
-//     // Get the current date
-//     var day = currentDate.getDate();
-//     var month = currentDate.getMonth() + 1; // Months are zero-indexed, so we add 1
-//     var year = currentDate.getFullYear();
-
-//     // Get the current time
-//     var hours = currentDate.getHours();
-//     var minutes = currentDate.getMinutes();
-//     var seconds = currentDate.getSeconds();
-
-//     // Format the date and time
-//     var formattedDate = `${year}-${month}-${day}`;
-//     var formattedTime = `${hours}:${minutes}:${seconds}`;
-
-//     // Output the current date and time
-//     console.log("Current Date:", formattedDate);
-//     console.log("Current Time:", formattedTime);
-// }
---
+++ stockPage/stockPage.js
@@ -0,0 +1,37 @@
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() 
+{
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+  
+
+function openLinkInNewTab(url) 
+{
+  var a = document.createElement("a");
+  a.href = url;
+  a.target = "_blank"; // Open in a new tab
+  // Simulate a click event
+  var evt = new MouseEvent("click", {
+      bubbles: true,
+      cancelable: true,
+      view: window
+  });
+  
+  a.dispatchEvent(evt);
+}
+
+window.addEventListener('load', getYear);
+function getYear()
+{
+  // Create a new Date object
+  var currentDate = new Date();
+  var year = currentDate.getFullYear();
+
+  var yearPlus100 = year + 100;
+
+  var updateYearInRow = document.getElementById("updateYearRow");
+  updateYearInRow.innerHTML = "Durability is paramount in my analysis. I ask myself, \"If I were in a coma for 100 years, would this company still thrive?\" This naturally " 
+  + "leads me towards strong brands with established consumer loyalty and enduring business models. Think of Starbucks' or McDonalds' enduring appeal. If I woke up in the year " + yearPlus100 
+  + " I would be shocked if these companies were not still around paying dividends to their shareholders."
+}
