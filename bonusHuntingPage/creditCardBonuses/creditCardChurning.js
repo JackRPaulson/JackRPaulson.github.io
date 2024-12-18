295b72b5f554d00897bbb420637b6269b84afe30
Jack <Paulsjac000@gmail.com>
2024-12-17 19:27:24 -0500

    "finished" credit card bonus page

---
+++ bonusHuntingPage/creditCardBonuses/creditCardChurning.js
@@ -0,0 +1,88 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const DiscoverItChromeImage = document.getElementById("DiscoverItChromeImage");
+    const ChaseAmazonPrimeImage = document.getElementById("ChaseAmazonPrimeImage");
+    // const ChaseAmazonPrimeWholeFoodsImage = document.getElementById("ChaseAmazonPrimeWholeFoodsImage");
+    const ChaseFreedomFlexImage = document.getElementById("ChaseFreedomFlexImage");
+    const ChaseFreedomUnlimitedImage = document.getElementById("ChaseFreedomUnlimitedImage");
+    const AMEXBCEImage = document.getElementById("AMEXBCEImage");
+    const HiltonHonorsImage = document.getElementById("HiltonHonorsImage");
+    const AMEXGoldImage = document.getElementById("AMEXGoldImage");
+    const AAATravelImage = document.getElementById("AAATravelImage");
+    const CitiCustomCashImage = document.getElementById("CitiCustomCashImage");
+    const AMEXHiltonSurpassImage = document.getElementById("AMEXHiltonSurpassImage");
+    const RobinhoodGoldCardImage = document.getElementById("RobinhoodGoldCardImage");
+    
+    DiscoverItChromeImage.addEventListener("click", function () 
+    {
+        const url = "https://refer.discover.com/s/paulsjac000?advocate.partner_share_id=7305981829";
+        window.open(url, "_blank");
+    });
+
+    ChaseAmazonPrimeImage.addEventListener("click", function () 
+    {
+        const url = "https://www.amazon.com/dp/BT00LN946S?externalReferenceId=42d266ea-6edf-4de9-965d-b2310af4a856";
+        window.open(url, "_blank");
+    });
+    
+    // ChaseAmazonPrimeWholeFoodsImage.addEventListener("click", function () 
+    // {
+    //     const url = "https://www.amazon.com/dp/BT00LN946S?externalReferenceId=42d266ea-6edf-4de9-965d-b2310af4a856";
+    //     window.open(url, "_blank");
+    // });
+
+    ChaseFreedomFlexImage.addEventListener("click", function () 
+    {
+        const url = "https://www.referyourchasecard.com/18N/6TC77OQ7ZM";
+        window.open(url, "_blank");
+    });
+
+    ChaseFreedomUnlimitedImage.addEventListener("click", function () 
+    {
+        const url = "https://www.referyourchasecard.com/18N/6TC77OQ7ZM";
+        window.open(url, "_blank");
+    });
+
+    AMEXBCEImage.addEventListener("click", function () 
+    {
+        const url = "https://americanexpress.com/en-us/referral/blue-cash-everyday-credit-card?ref=JACKPcCuq&XL=MIMNS";
+        window.open(url, "_blank");
+    });
+
+    HiltonHonorsImage.addEventListener("click", function () 
+    {
+        const url = "https://www.americanexpress.com/en-us/credit-cards/referral/prospect/hilton-honors?ref=JACKPFNge&XL=MIMNS";
+        window.open(url, "_blank");
+    });
+
+    AMEXGoldImage.addEventListener("click", function () 
+    {
+        const url = "https://americanexpress.com/en-us/referral/gold-card?ref=JACKP555t&XL=MIMNS";
+        window.open(url, "_blank");
+    });
+
+    AAATravelImage.addEventListener("click", function () 
+    {
+        const url = "https://northeast.aaa.com/financial/more-products/aaa-visa-travel-advantage-card.html";
+        window.open(url, "_blank");
+    });
+
+    CitiCustomCashImage.addEventListener("click", function () 
+    {
+        const url = "https://www.citi.com/usc/LPACA/Citi/Cards/CustomCash/ps/index.html?cmp=knc|acquire|2006|CARDS|Google|BR&gclid=EAIaIQobChMI-pjhiqipigMVhTcIBR1qWSOVEAAYASAAEgK7pvD_BwE&gclsrc=aw.ds&ProspectID=PnCOpK25M12FDsQsgRq0oKPHmg2gOtiS";
+        window.open(url, "_blank");
+    });
+
+    AMEXHiltonSurpassImage.addEventListener("click", function () 
+    {
+        const url = "https://www.americanexpress.com/us/credit-cards/card/hilton-honors-surpass/?eep=25330&intlink=US-Axp-Shop-Consumer-VAC-HiltonSurpass-Prospect-Cardname-CardTile&inav=menu_cards_pc_travelrewardscards";
+        window.open(url, "_blank");
+    });
+
+    RobinhoodGoldCardImage.addEventListener("click", function () 
+    {
+        const url = "https://robinhood.com/creditcard/";
+        window.open(url, "_blank");
+    });
+  
+
+  });
