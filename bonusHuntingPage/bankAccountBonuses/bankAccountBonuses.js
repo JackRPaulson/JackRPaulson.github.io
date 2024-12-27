0794724ffbd17778366660918939a57beffaff65
Jack <Paulsjac000@gmail.com>
2024-12-26 22:10:49 -0500

    adding back button bar to the credit card and bank bonus page

--- bonusHuntingPage/bankAccountBonuses/bankAccountBonuses.js
+++ bonusHuntingPage/bankAccountBonuses/bankAccountBonuses.js
@@ -21,3 +21,10 @@ document.addEventListener("DOMContentLoaded", function () {
         window.open(url, "_blank");
     });
   });
+
+  // When the user clicks on the button, scroll to the top of the document
+function topFunction() 
+{
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
