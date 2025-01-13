278bedbff558b9d511b959a076a7d18daeecf760
Jack <Paulsjac000@gmail.com>
2025-01-12 19:11:06 -0500

    updated Referral Links

--- bonusHuntingPage/creditCardBonuses/creditCardChurning.js
+++ bonusHuntingPage/creditCardBonuses/creditCardChurning.js
@@ -1,22 +1,88 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // const DiscoverItChromeImage = document.getElementById("DiscoverItChromeImage");
-    // const ChaseAmazonPrimeImage = document.getElementById("ChaseAmazonPrimeImage");
-    
-    // DiscoverItChromeImage.addEventListener("click", function () 
-    // {
-    //     const url = "https://refer.discover.com/s/paulsjac000?advocate.partner_share_id=7305981829";
-    //     window.open(url, "_blank");
-    // });
+  const DiscoverItChromeImage = document.getElementById("DiscoverItChromeImage");
+    const ChaseAmazonPrimeImage = document.getElementById("ChaseAmazonPrimeImage");
+    // const ChaseAmazonPrimeWholeFoodsImage = document.getElementById("ChaseAmazonPrimeWholeFoodsImage");
+    const ChaseFreedomFlexImage = document.getElementById("ChaseFreedomFlexImage");
+    const ChaseFreedomUnlimitedImage = document.getElementById("ChaseFreedomUnlimitedImage");
+    const AMEXBCEImage = document.getElementById("AMEXBCEImage");
+    const HiltonHonorsImage = document.getElementById("HiltonHonorsImage");
+    const AMEXGoldImage = document.getElementById("AMEXGoldImage");
+    const AAATravelImage = document.getElementById("AAATravelImage");
+    const CitiCustomCashImage = document.getElementById("CitiCustomCashImage");
+    const AMEXHiltonSurpassImage = document.getElementById("AMEXHiltonSurpassImage");
+    const RobinhoodGoldCardImage = document.getElementById("RobinhoodGoldCardImage");
 
-    // ChaseAmazonPrimeImage.addEventListener("click", function () 
+    DiscoverItChromeImage.addEventListener("click", function () 
+    {
+      const url = "https://refer.discover.com/s/paulsjac000?advocate.partner_share_id=7305981829";
+      window.open(url, "_blank");
+    });
+
+    ChaseAmazonPrimeImage.addEventListener("click", function () 
+    {
+      const url = "https://www.amazon.com/dp/BT00LN946S?externalReferenceId=42d266ea-6edf-4de9-965d-b2310af4a856";
+      window.open(url, "_blank");
+    });
+
+    // ChaseAmazonPrimeWholeFoodsImage.addEventListener("click", function () 
     // {
     //     const url = "https://www.amazon.com/dp/BT00LN946S?externalReferenceId=42d266ea-6edf-4de9-965d-b2310af4a856";
     //     window.open(url, "_blank");
     // });
-    
- 
-  
 
+    ChaseFreedomFlexImage.addEventListener("click", function () 
+    {
+      const url = "https://www.referyourchasecard.com/18J/6Z5A9HQFC5";
+      window.open(url, "_blank");
+    });
+
+    ChaseFreedomUnlimitedImage.addEventListener("click", function () 
+    {
+      const url = "https://www.referyourchasecard.com/18J/6Z5A9HQFC5";
+      window.open(url, "_blank");
+    });
+
+    AMEXBCEImage.addEventListener("click", function () 
+    {
+      const url = "https://americanexpress.com/en-us/referral/blue-cash-everyday-credit-card?ref=JACKPL2SW&XLINK=MYCP";
+      window.open(url, "_blank");
+    });
+
+    HiltonHonorsImage.addEventListener("click", function () 
+    {
+      const url = "https://americanexpress.com/en-us/referral/hilton-honors?ref=JACKP9L7B&XLINK=MYCP";
+      window.open(url, "_blank");
+    });
+
+    AMEXGoldImage.addEventListener("click", function () 
+    {
+      const url = "https://americanexpress.com/en-us/referral/gold-card?ref=JACKPJU4S&XLINK=MYCP";
+      window.open(url, "_blank");
+    });
+
+    AAATravelImage.addEventListener("click", function () 
+    {
+      const url = "https://www.nerdwallet.com/article/credit-cards/aaa-credit-card";
+      window.open(url, "_blank");
+    });
+
+    CitiCustomCashImage.addEventListener("click", function () 
+    {
+      const url = "https://www.citi.com/usc/LPACA/Citi/Cards/CustomCash/ps/index.html?cmp=knc|acquire|2006|CARDS|Google|BR&gclid=EAIaIQobChMI-pjhiqipigMVhTcIBR1qWSOVEAAYASAAEgK7pvD_BwE&gclsrc=aw.ds&ProspectID=PnCOpK25M12FDsQsgRq0oKPHmg2gOtiS";
+      window.open(url, "_blank");
+    });
+
+    AMEXHiltonSurpassImage.addEventListener("click", function () 
+    {
+      const url = "https://americanexpress.com/en-us/referral/hilton-honors?ref=JACKPdgUp&xl=cp108k";
+      window.open(url, "_blank");
+    });
+
+    RobinhoodGoldCardImage.addEventListener("click", function () 
+    {
+      const url = "https://robinhood.com/creditcard/";
+      window.open(url, "_blank");
+    });
   });
 
   // When the user clicks on the button, scroll to the top of the document
